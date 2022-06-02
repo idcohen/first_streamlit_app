1209800198458f3d1d7c090578c1ab86f8d03320
Dov Cohen <idcohen@users.noreply.github.com>
2022-06-02 08:15:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # fruit picker
-streamlit.multiselect("select a fruit",list(my_fruit_list.index),['Advocado','Strawberries'])
+streamlit.multiselect("select a fruit",list(my_fruit_list.index))
+#,['Advocado','Strawberries'])
 
 # display fruits
 streamlit.dataframe(my_fruit_list)
