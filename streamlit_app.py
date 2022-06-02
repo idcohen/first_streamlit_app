f1277145c84682dcadb39fd739af2ec840026d7e
Dov Cohen <idcohen@users.noreply.github.com>
2022-06-02 08:11:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # fruit picker
 streamlit.multiselect('select a fruit',list(my_fruit_list.index))
 
