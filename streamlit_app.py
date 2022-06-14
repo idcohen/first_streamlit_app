dccb1028a9db93604d887210a78138beccfa35dd
Dov Cohen <idcohen@users.noreply.github.com>
2022-06-14 06:58:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_row)
+add_my_fruit = streamlit.text_input("select a fruit")
+streamlit.write("user entered",add_my_fruit)
 
 streamlit.title("This is a test")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
