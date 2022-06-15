ec1c1ed1367be0b1e9a5368103a01fbe5ceae95e
Dov Cohen <idcohen@users.noreply.github.com>
2022-06-14 19:15:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.dataframe(my_data_row)
 
 def insert_row_snfl(new_fruit):
   with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')");
+      my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('" + new_fruit + "')");
   return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input("select a fruit")
